bad6c17cf4285eeb5ff2422eac692065a78e5833
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2023-10-24 09:44:36 +0545

    clean up compoenents

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,7 +1,6 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Link from "next/link";
-import { Button } from "./ui/button";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
@@ -21,7 +20,6 @@ const Nav = () => {
             onClick={() => {
               setTheme("dark");
             }}
-            // className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
             className=" h-[1.2rem] w-[1.2rem] rotate-0"
           />
         ) : (
@@ -29,13 +27,9 @@ const Nav = () => {
             onClick={() => {
               setTheme("light");
             }}
-            // className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
             className=" h-[1.2rem] w-[1.2rem] rotate-0 "
           />
         )}
-        {/* <Link href={"/"} className="text-sm">
-          <Button>Contact</Button>
-        </Link> */}
       </div>
     </div>
   );
--- components/ui/card.jsx
+++ components/ui/card.jsx
@@ -5,19 +5,19 @@ const Card = ({ blog }) => {
   return (
     <>
       <Link href={`/blog/${blog.slug}`}>
-        <div class="bg-white w-full max-w-screen-lg mx-auto border-t-4 border-blue-500 p-4 sm:p-6 pb-8 pt-6 rounded-lg shadow-lg mb-10">
-          <div class="mb-4 ">
-            <span class="inline-block py-1 px-2 border border-blue-500 rounded-md text-blue-500 text-xs sm:text-sm font-semibold hover:bg-blue-500 hover:text-white hover:border-transparent transition duration-300 ease-in-out">
+        <div className="bg-white w-full max-w-screen-lg mx-auto border-t-4 border-blue-500 p-4 sm:p-6 pb-8 pt-6 rounded-lg shadow-lg mb-10">
+          <div className="mb-4 ">
+            <span className="inline-block py-1 px-2 border border-blue-500 rounded-md text-blue-500 text-xs sm:text-sm font-semibold hover:bg-blue-500 hover:text-white hover:border-transparent transition duration-300 ease-in-out">
               {blog.topics[0].name}
             </span>
           </div>
-          <h2 class="text-2xl sm:text-3xl  font-extrabold text-gray-900 mb-4">
+          <h2 className="text-2xl sm:text-3xl  font-extrabold text-gray-900 mb-4">
             {blog.title}
           </h2>
-          <p class="text-gray-800 text-base sm:text-md mb-4">
+          <p className="text-gray-800 text-base sm:text-md mb-4">
             {blog.previewText}
           </p>
-          <span class="text-blue-500 font-semibold hover:underline">
+          <span className="text-blue-500 font-semibold hover:underline">
             Read more...
           </span>
         </div>
